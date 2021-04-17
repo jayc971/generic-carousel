f18b7b6f4b449c360dc207361a69c88622d3d517
jayc971 <jayanika@serviceform.com>
2021-04-17 11:08:38 +0530

    fdsfdsf

--- text_based_slider/flickity.pkgd.js
+++ text_based_slider/flickity.pkgd.js
@@ -1019,7 +1019,7 @@ proto.positionSlider = function() {
     this.shiftWrapCells( x );
   }
 
-  this.setTranslateX( x - 17, this.isAnimating);
+  this.setTranslateX( x - 35, this.isAnimating);
   this.dispatchScrollEvent();
 };
 
